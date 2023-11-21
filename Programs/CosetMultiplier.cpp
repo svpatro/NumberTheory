a7d3671d15148a719816c0651e46ff2c3e51dbd8
Shamik Patro <patroshamik@gmail.com>
2023-11-20 20:44:16 -0600

    Added some code

--- Programs/CosetMultiplier.cpp
+++ Programs/CosetMultiplier.cpp
@@ -1,8 +1,24 @@
 // Author: Shamik V. Patro
 #include <iostream>
+#include <vector>
 
 int main(){
 
+    int n = 0; // Integers modulo n
+    int H_Size = 0; // Subgroup size
+
+    std::cout << "What is the modulo?: ";
+    std::cin >> n;
+    std::cout << "What is the size (order) of the original coset?: ";
+    std::cin >> H_Size;
+
+    std::vector<int> d_cosets; // Number of distinct cosets
+
+    for (int i = 0; i < H_Size; i++){
+
+        
+
+    }
 
     return 0;
 
